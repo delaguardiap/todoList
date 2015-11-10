48c6d63fc30bef9ecf5828c3bc97c58ab5fc8ce1
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2015-11-10 14:36:22 -0500

    Add inital work on todo list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :tasks
+  root 'tasks#index'
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # See how all y1our routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
